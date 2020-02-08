049b1dd5ea5019cda786bc943b9e99c96b819bc0
chamodib <46634954+chamodib@users.noreply.github.com>
2020-02-07 18:35:53 -0800

    Update a4.cpp

--- a4.cpp
+++ a4.cpp
@@ -1,24 +1,6 @@
 //a4.cpp
-/////////////////////////////////////////////////////////////////////////
-//
-// Student Info
-// ------------
-//
 // Name : Chamodi Basnayake
-//
-//
-// Statement of Originality
-// ------------------------
-//
-// All the code and comments below are my own original work. For any non-
-// original work, I have provided citations in the comments with enough
-// detail so that someone can see the exact source and extent of the
-// borrowed work.
-//
-// In addition, I have not shared this work with anyone else, and I have
-// not seen solutions from other students, tutors, websites, books,
-// etc.
-//
+
 /////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
